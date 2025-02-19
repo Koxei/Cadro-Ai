4b76b7de28b4aa5de0c7368ee65e94e40a2d962e
Koxei <ciekawe320@gmail.com>
2025-02-19 01:40:21 +0100

    script added

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,7 +3,7 @@
 # Navigate to the script's directory
 cd "$(dirname "$0")"/..
 
-# Find and remove node_modules directories and pnpm-lock.yaml files
+# Find and remove node_modules directories and pnpm-lock.yaml file
 find . -type d -name "node_modules" -exec rm -rf {} + \
     -o -type f -name "pnpm-lock.yaml" -exec rm -f {} +
 
